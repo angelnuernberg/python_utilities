5227caf2ee9aade5d83aa09bb693c22431134ec9
lenny <lenny@gld.de>
2022-11-16 11:07:01 +0100

    Refactored files, extracted logging utility -> Not refactored to class as the benefit is not that big in that case (maybe for csv_utilities could be useful)

--- register_to_gdrive.py
+++
@@ -1,9 +0,0 @@
-from pydrive2.auth import GoogleAuth
-
-gauth=GoogleAuth()
-gauth.LocalWebserverAuth()
-
-# This needs to be done the first time -> a browser opens, you enter your google
-# login, you need to confirm to trust the app
-# If it worked OK, you get the message "Authentication successful"
-# however, this authentication is temporal : to persist credentials, a settings.yaml file needs to be defined
--- zip_utilities.py
+++ zip_utilities.py
@@ -3,11 +3,12 @@ import zipfile
 import config
 import pandas as pd
 import logging
+import logging_utility
 
 # Very useful: https://realpython.com/python-zipfile/
 # EXAMPLE OF ZIPPING A FOLDER
 
-def configure_logging(logfile):
+# def configure_logging(logfile):
     # https: // realpython.com / python - logging /
     # logging.debug('This is a debug message')
     # logging.info('This is an info message')
@@ -20,17 +21,21 @@ def configure_logging(logfile):
 
     # Create a custom logger
     # From https://realpython.com/python-logging/
-    logger_intern=logging.getLogger()
-    logger_intern.setLevel(logging.INFO)
-    file_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    file_handler=logging.FileHandler(logfile, 'w')
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(file_format)
-    logger_intern.addHandler(file_handler)
-    return logger_intern
+#    logger_intern=logging.getLogger()
+#    logger_intern.setLevel(logging.INFO)
+#    file_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+#    file_handler=logging.FileHandler(logfile, 'w')
+ #   file_handler.setLevel(logging.INFO)
+  #  file_handler.setFormatter(file_format)
+#    logger_intern.addHandler(file_handler)
+ #   return logger_intern
+
+# TODO: Refactoring to CLASS?: to have as inner property or in constructor
+#  the logger, but the benefit is not that big as logger is accessed staticly...
 
 
-def fetchDirectoriesToZip(fileListDirectories):
+
+def fetchListDirectoriesFromFile(fileListDirectories):
     file=open(fileListDirectories,'r')
     # return file.readlines()
     #   This works OK!!
@@ -38,6 +43,8 @@ def fetchDirectoriesToZip(fileListDirectories):
     dataframe = pd.read_csv(fileListDirectories, delimiter=",")
     listDirectories = dataframe["directory"]
     # print(f'Display column with name: directory:\n\r{listDirectories}')
+    logging.info("--------- fetchListDirectoriesFromFile -------------")
+    logging.info(f'Display column with name: directory -> List of files to process: {listDirectories.to_list()}')
     return listDirectories.to_list()
 
 def zipDirectory(directoryPath):
@@ -50,6 +57,7 @@ def zipDirectory(directoryPath):
 # This works great!!
 
 def printDirZipFile(directoryPath):
+    logging.info("--------- printDirZipFile -------------")
     directory = pathlib.Path(directoryPath)
     path_to_zip_file = pathlib.Path(str(directory) + '.zip')
     with zipfile.ZipFile(path_to_zip_file, mode="r") as archive:
@@ -64,7 +72,7 @@ def printDirZipFile(directoryPath):
 
 
 def zipDirectoriesFromFile(fileToDirectoriesFile):
-    listDirectories=fetchDirectoriesToZip(fileToDirectoriesFile)
+    listDirectories=fetchListDirectoriesFromFile(fileToDirectoriesFile)
     logging.info(f"List of directories to zip: {listDirectories}")
     for directory in listDirectories:
         zipDirectory(directory)
@@ -74,19 +82,21 @@ def zipDirectoriesFromFile(fileToDirectoriesFile):
         logging.info("----------------------------------")
     logging.info("All folders zipped correctly")
 
-try:
-    logger = configure_logging(config.log_file)
-    logger.info("Testing the logger")
-    zipDirectoriesFromFile('directoriesList.txt')
-    logger.info("Finished OK")
-except Exception as e:
-    logging.exception(e)
-# Check that this works OK:
-    # zipDirectory(r'U:\test_intruntime_u')
-    # printDirZipFile(r'U:\test_intruntime_u.zip')
+def main_method():
+    try:
+        logger = logging_utility.configure_logging(config.log_file)
+        logger.info("Testing the logger")
+        zipDirectoriesFromFile('directoriesList.txt')
+        logger.info("Finished OK")
+    except Exception as e:
+        logging.exception(e)
+    # Check that this works OK:
+        # zipDirectory(r'U:\test_intruntime_u')
+        # printDirZipFile(r'U:\test_intruntime_u.zip')
 
 # folder_to_zip=r'U:\0_cc-pj-con'
 # path_to_zip_file=r'U:\directory.zip'
 # print(directory)
 # print(path_to_zip_file)
 
+# main_method()
